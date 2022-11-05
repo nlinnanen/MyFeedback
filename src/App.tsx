f0c61014977c333b2c4b4c54b1dac850da5df0e8
Niklas Linnanen <nlinnanen@gmail.com>
2022-11-05 20:50:03 +0200

    Try to fix styles and firebase config
    Co-authored-by: Mikael Uusitalo <mUusitalo@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import Questionare from './components/Questionare'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/admin" element={<div className="container">Admin</div>} />
-      <Route
-        path="/"
-        element={
-          <div className="container">
-            <Questionare />
-            <input />
-          </div>
-        }
-      />
-    </Routes>
+    <div className="top">
+      <Routes>
+        <Route path="/admin" element={<div className="container">Admin</div>} />
+        <Route
+          path="/"
+          element={
+            <div className="container">
+              <Questionare />
+              <input />
+            </div>
+          }
+        />
+      </Routes>
+    </div>
   )
 }
 
--- src/components/Questionare/index.tsx
+++ src/components/Questionare/index.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react"
-import AnswerButton from "./AnswerButton"
-import "./Questionare.scss"
+import React, { useState } from 'react'
+import AnswerButton from './AnswerButton'
+import './Questionare.scss'
 
 const ANSWERS = ['toinen', 'kolmas', 'neljäs', 'viides']
 const Questionare = () => {
-  const [hasAnswered, setHasAnswered] = useState<boolean>(false);
+  const [hasAnswered, setHasAnswered] = useState<boolean>(false)
   return (
     <div className="grid">
-      {!hasAnswered && ANSWERS.map((a, i) => (
-        <AnswerButton key={i} text={a} onClick={() => setHasAnswered(true)}/>
-      ))}
+      {!hasAnswered && (
+        <div>
+          {' '}
+          {ANSWERS.map((a, i) => (
+            <AnswerButton key={i} text={a} onClick={() => setHasAnswered(true)} />
+          ))}
+        </div>
+      )}
+      {hasAnswered && <div>tulokset</div>}
     </div>
   )
 }
